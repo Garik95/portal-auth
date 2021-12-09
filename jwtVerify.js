18080635c756d625988481d129f3dbfb14fb28a4
Garik95 <Garik95@gmail.com>
2021-12-09 11:06:05 +0500

    removed test functions

--- jwtVerify.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjE3Mjk3MGQwMjg5MjFmY2Y4MGIyZTRjIiwiZW1haWwiOiJ1c2VyQG1haWwuY29tIiwiaWF0IjoxNjM3NzMzOTAzfQ.72eu3R7IYw6FCHO7HaI6qmbsEaSG5GZSp9aPbU8Fv-c';
-
-var decoded = jwt.verify(token, 'IPAKPASSSECRET');
-
-console.log(decoded);
